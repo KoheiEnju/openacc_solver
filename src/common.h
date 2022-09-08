ce2e2fadab6c3e1ceab17b77bb9f24423a94e602
hyperenju <hyperenju@gmail.com>
2022-09-09 05:21:41 +0900

    add common.h

---
+++ src/common.h
@@ -0,0 +1,13 @@
+//
+// Created by kohei on 22/09/09.
+//
+
+#ifndef OPENACC_SOLVER_COMMON_H
+#define OPENACC_SOLVER_COMMON_H
+
+#endif //OPENACC_SOLVER_COMMON_H
+
+#include <vector>
+#include <iostream>
+
+using v2 = std::vector<std::vector<float>>;
